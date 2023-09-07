4f03ee25606fdc6a9b1a4fede21cddc6b8ce519c
Omid Mortazavi <omidmor@students.cs.ubc.ca>
2023-09-06 19:37:42 -0700

    Initial Commit

---
+++ Clickure/ClickureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickureApp.swift
+//  Clickure
+//
+//  Created by Seyed Omid Mortazavi on 2023-09-06.
+//
+
+import SwiftUI
+
+@main
+struct ClickureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clickure/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Clickure
+//
+//  Created by Seyed Omid Mortazavi on 2023-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
